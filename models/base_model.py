7a3ad76ade8c6ef866cded730e635f3309396a21
Coconuts-del <6712@holbertonstudents.com>
2023-11-01 09:04:02 +0100

    updated base_model and test

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" 
-a class BaseModel that defines all common 
-attributes/methods for other classes 
+"""
+a class BaseModel that defines all common
+attributes/methods for other classes
 """
 
 import uuid
@@ -9,17 +9,17 @@ from datetime import datetime
 
 
 class BaseModel:
-    """ base class for managing common attributes and methods for derived classes """
+    """ base class for managing common attributes and methods
+        for derived classes """
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """ Initialize a new BaseModel instance with 
-        a unique ID, creation, and update timestamps
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """ Return a string representation of the object """
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -28,9 +28,7 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Return a dictionary representation of the object
-        """
+        """ Return a dictionary representation of the object """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,44 +1,57 @@
 import unittest
 from unittest.mock import patch
 from datetime import datetime
-from models.base_model import BaseModel  # Import the BaseModel class from your actual module
+from models.base_model import BaseModel
+""" Import the BaseModel class from your actual module """
 
 
 class TestBaseModel(unittest.TestCase):
 
     @patch('models.base_model.datetime')
     def test_init(self, mock_datetime):
+        """ Mock the current date and time for testing """
         mock_now = datetime(2023, 10, 31, 12, 0, 0)
         mock_datetime.now.return_value = mock_now
 
+        """  Create an instance of the BaseModel class """
         base_model = BaseModel()
 
+        """  Check that the instance has a non-empty ID
+            the correct creation and update timestamps """
         self.assertIsNotNone(base_model.id)
         self.assertEqual(base_model.created_at, mock_now)
         self.assertEqual(base_model.updated_at, mock_now)
 
     def test_save(self):
+        """  Create an instance of the BaseModel class """
         base_model = BaseModel()
+        """ Store the original 'updated_at' timestamp """
         original_updated_at = base_model.updated_at
+        """ Call the 'save' method to update the 'updated_at' timestamp """
         base_model.save()
-
+        """ Check that 'updated_at' has been updated """
         self.assertNotEqual(base_model.updated_at, original_updated_at)
 
     def test_to_dict(self):
+        """ convert the instance to a dictionary """
         base_model = BaseModel()
         base_model_dict = base_model.to_dict()
 
+        """ Check if the necessary keys are present in the dictionary """
         self.assertTrue('__class__' in base_model_dict)
         self.assertTrue('created_at' in base_model_dict)
         self.assertTrue('updated_at' in base_model_dict)
 
     def test_str(self):
+        """ convert the instance to a string representation """
         base_model = BaseModel()
         obj_str = str(base_model)
 
+        """ Check if the class name and ID are present
+             in the string representation """
         self.assertTrue(base_model.__class__.__name__ in obj_str)
         self.assertTrue(base_model.id in obj_str)
 
+
 if __name__ == '__main__':
     unittest.main()
-
