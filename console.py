b95be6781561706cf538b4c92b839e99dbf5ad36
Davis <davisjoseph767@gmail.com>
2023-11-01 07:05:26 -0700

    Wrote a program called console.py that contains the entry point of the command interpreter:

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, line):
+        """
+        Quit the command interpreter when EOF is encountered.
+        """
+        print()
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing on an empty line (just pressing ENTER).
+        """
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
