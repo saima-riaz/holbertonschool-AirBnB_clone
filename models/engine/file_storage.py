b3d4418e989db85b660d8f686bb7d98846d5dc23
Coconuts-del <6712@holbertonstudents.com>
2023-11-02 19:49:35 +0100

    merge issue

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """ Creation to define attrs methods of the console """
 import json
-import os
+from os import path
 from models.base_model import BaseModel
-from models.state import State
+"""from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from datetime import datetime
-
 classes = {"BaseModel": BaseModel, "User": User, "Place": Place,
-           "State": State, "City": City, "Amenity": Amenity, "Review": Review}
+           "State": State, "City": City, "Amenity": Amenity, "Review": Review}"""
+
+
+classes = {"BaseModel": BaseModel}
 
 
 class FileStorage:
