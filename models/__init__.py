b6e1943b4a5916732203156a64bfa624c26f57c6
Coconuts-del <6712@holbertonstudents.com>
2023-11-03 11:59:08 +0100

    update _init__

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,12 @@
 """ __init__ magic method for models directory """
 
 from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 storage = FileStorage()
 storage.reload()
